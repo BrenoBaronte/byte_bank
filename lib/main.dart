dafb130194d33a9533ab5065d3a0facf563bbb15
Breno Baronte <brenobaronte@hotmail.com>
2020-05-22 00:46:02 -0300

    enabling hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: TransferList(),
-          appBar: AppBar(
-            title: Text('Transferências'),
-          ),
-          floatingActionButton: FloatingActionButton(child: Icon(Icons.add)),
-        ),
+void main() => runApp(ByteBankApp());
+
+class ByteBankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: TransferForm(),
       ),
     );
+  }
+}
+
+class TransferForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Ordering Tranfer'),
+        ),
+        body: Text('dfdfdfds'));
+  }
+}
 
 class TransferList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TransferItem(Transfer(value: 100.0, accountNumber: 1001)),
-        TransferItem(Transfer(value: 200.0, accountNumber: 1001)),
-        TransferItem(Transfer(value: 300.0, accountNumber: 2002)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: <Widget>[
+          TransferItem(Transfer(value: 100.0, accountNumber: 1001)),
+          TransferItem(Transfer(value: 200.0, accountNumber: 1001)),
+          TransferItem(Transfer(value: 300.0, accountNumber: 2002)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.add)),
     );
   }
 }
 
 class TransferItem extends StatelessWidget {
-
   final Transfer _transfer;
 
-  TransferItem( this._transfer );
+  TransferItem(this._transfer);
 
   @override
   Widget build(BuildContext context) {
@@ -47,5 +64,5 @@ class Transfer {
   final double value;
   final int accountNumber;
 
-  Transfer({ this.value, this. accountNumber });
+  Transfer({this.value, this.accountNumber});
 }
