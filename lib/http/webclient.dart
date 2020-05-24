c3dbe2dbea3785d96bdbad60c69c74db1a35e8bb
Breno Baronte <brenobaronte@hotmail.com>
2020-05-23 23:13:35 -0300

    covering possible problems

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -8,8 +8,9 @@ Future<List<Transaction>> findAll() async {
   final Client client = HttpClientWithInterceptor.build(
     interceptors: [LoggingInterceptor()],
   );
-  final Response response =
-      await client.get('http://172.17.250.1:8080/transactions');
+  final Response response = await client
+      .get('http://172.17.250.1:8080/transactions')
+      .timeout(Duration(seconds: 5));
   final List<dynamic> decodedJson = jsonDecode(response.body);
   final List<Transaction> transactions = List();
   for (Map<String, dynamic> transactionJson in decodedJson) {
